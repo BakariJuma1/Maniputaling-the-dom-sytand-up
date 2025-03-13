8acae38005569f017a5e8748e99c49bc23be68a1
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-03-13 11:52:39 +0300

    events in js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 let firstName = "Bakari Juma";
 let job = "jnr Dev";
 let hired = "You have hired bakari";
+let skills = ["HTML", "CSS", "JavaScript"];
+let projects = "link";
+let contact = "calling";
+
 //changing or writing into the html page using the inner.html method adding the value of my p to firstnamme+ job
-document.getElementById(
-  "myP"
-).innerHTML = `my name is ${firstName}  a  ${job} `;
+document.getElementById("myP").innerHTML = `Name: ${firstName} `;
+
+document.getElementById("myP2").innerHTML = `Job: ${job} `;
+document.getElementById("mySpan1").innerHTML = `skills: ${skills}`;
+
+document.getElementById("mySpan2").innerHTML = `projects: ${projects}`;
 
 document.getElementById("myButton").addEventListener("click", function () {
   document.getElementById("thanks").innerHTML = hired;
 });
+
+function openEmail() {
+  window.location.href = "mailto:jumaisaq@gmail.com";
+}
+document.getElementById("contactButton").addEventListener("click", openEmail);
